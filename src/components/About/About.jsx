0bfc7b36f4fde082c66f2079e7a405e18b8c4666
becp12 <rebecca-preece@outlook.com>
2023-09-23 17:17:25 -0700

    Separated constant variables from component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,66 +1,8 @@
 import { Link } from "react-router-dom";
 import './About.css';
 import Carousel from "../Carousel/Carousel";
+import * as Constants from "../../constants/data";
 export default function About() {
-
-    const projectScreenshots = [
-        {
-            placeholderImage: "images/placeholder1.png",
-            isActive: true
-        },{
-            placeholderImage: "images/placeholder2.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/placeholder3.png",
-            isActive: false
-        },{
-            placeholderImage: "images/placeholder3.png",
-            isActive: false
-        }]
-
-    const yarnStashScreenshots = [
-        {
-            placeholderImage: "images/yarn-stash/grey.jpg",
-            isActive: true
-        },{
-            placeholderImage: "images/yarn-stash/pink.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/yarn-stash/purple.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/yarn-stash/red.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/yarn-stash/teal.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/yarn-stash/yellow.jpg",
-            isActive: false
-        }]
-
-    const patternScreenshots = [
-        {
-            placeholderImage: "images/patterns/pillow1.jpg",
-            isActive: true
-        },{
-            placeholderImage: "images/patterns/pillow2.jpg",
-            isActive: false
-        },{
-            placeholderImage: "images/patterns/pillow3.jpg",
-            isActive: false
-        }
-    ]
-
-    const swatchScreenshots = [
-        {
-            placeholderImage: "images/stitches/stitch2.webp",
-            isActive: true
-        },{
-            placeholderImage: "images/stitches/stitch3.webp",
-            isActive: false
-        }
-    ]
 
     return (
       <main className="container-sm">
@@ -73,16 +15,16 @@ export default function About() {
         <p>In the loop is the perfect companion for your crochet and knit projects. You can track your projects, what you have in your yarn stash (coming soon), keep track of all your patterns in our Pattern Library (coming soon) and even explore new stiches in our Swatch Library (coming soon).</p>
         
         <h3>Project Tracking</h3>
-        <Carousel name={"ProjectCarousel"} screenshots={projectScreenshots}/>
+        <Carousel name={"ProjectCarousel"} screenshots={Constants.projectScreenshots}/>
 
         <h3>Yarn Stash</h3>
-        <Carousel name={"YarnCarousel"} screenshots={yarnStashScreenshots}/>
+        <Carousel name={"YarnCarousel"} screenshots={Constants.yarnStashScreenshots}/>
 
         <h3>Pattern Library</h3>
-        <Carousel name={"PatternCarousel"} screenshots={patternScreenshots}/>
+        <Carousel name={"PatternCarousel"} screenshots={Constants.patternScreenshots}/>
 
         <h3>Swatch Library</h3>
-        <Carousel name={"SwatchCarousel"} screenshots={swatchScreenshots}/>
+        <Carousel name={"SwatchCarousel"} screenshots={Constants.swatchScreenshots}/>
 
         <h4 className="text-center">Please login to access all the features of the app</h4>
         <Link to={'/login'}><center>Click here to Login</center></Link> {/* This is will link to the login page*/}
